bcd4a483d9e6587af8ecd14b60616f3250840428
Jannik Behrens <56647862+JannikBehrens@users.noreply.github.com>
2021-06-12 17:26:28 +0200

    Update updateUI.test.js

--- __test__/updateUI.test.js
+++ __test__/updateUI.test.js
@@ -1,10 +1,8 @@
 // ref: https://stackoverflow.com/questions/50818474/how-to-properly-test-if-the-type-of-the-result-is-a-javascript-function-in-jes
+import { updateUI } from "../src/client/js/updateUI"
 
-const requestPost = require('../src/client/js/updateUI')
-const updateUIFunction = requestPost.updateUI
-
-describe('Test "updateUI()" should be a function' , () => {
-    test('It should be a function', () => {
-        expect(typeof updateUIFunction).toBe("function");
+describe('updateUI() must be a function' , () => {
+    test('Should be function', () => {
+        expect(typeof updateUI).toBe("function");
     });
 })
